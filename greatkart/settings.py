a9c117df90ef07f4db4e42478553eede84113d70
Vineeth97Kumar <74662872+Vineeth97Kumar@users.noreply.github.com>
2021-07-18 10:46:54 +0530

    Masked Password

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -142,6 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vineethtuntun@gmail.com'
-EMAIL_HOST_PASSWORD = '8015550306'
+EMAIL_HOST_PASSWORD = 'xxxxxxxx'
 EMAIL_USE_TLS = True
 
